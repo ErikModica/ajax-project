19e1350a87a5c24423e7bfdccd3364922a541cac
ErikModica <erikmodica@gmail.com>
2021-02-24 17:35:13 -0800

    fixed a large error where a pokemon would come up more than one time on the same quiz. Instead of choosing a random value at the begining of the getPokemon function, now at the begining of the program, an array is made with shuffled values of the range of parameters.  Each time the user inputs the right answer, the index in incrimented by one, and the next pokemon on that index shows.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
 var rightAnswer = false;
+var currentID = 0;
+var randomIDList = shuffle(1, 10);
 var $pokemonImg = document.querySelector('.pokemon-img');
 var $answerBox = document.querySelector('.answer-input');
-var $answerButton = document.querySelector('button');
 
 addEventListener('load', getPokemon);
 $answerBox.addEventListener('keydown', getNextPokemon);
 
-
-function getRandomID(min, max) {
-  var randomVal = Math.random() * (max - min) + min;
-  return Math.round(randomVal);
-}
-
-
 function getPokemon() {
-  var randomID = getRandomID(1, 151);
   var pokemonName;
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + randomID);
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + randomIDList[currentID]);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     if (xhr.status !== 200) {
@@ -26,7 +19,7 @@ function getPokemon() {
 
     $pokemonImg.setAttribute('src', xhr.response.sprites.front_default);
 
-    var pokemonName = xhr.response.species.name;
+    pokemonName = xhr.response.species.name;
     if (pokemonName === 'nidoran-f' || pokemonName === 'nidoran-m') {
       pokemonName = 'nidoran';
     }
@@ -41,21 +34,40 @@ function getPokemon() {
         console.log('ur sicckk dude')
         rightAnswer = true;
         $answerBox.className = 'answer-input right';
-
+        console.log(rightAnswer);
       }
-
     });
-
-
   });
   xhr.send();
 }
 
 function getNextPokemon(event) {
   if (rightAnswer && event.key === 'Enter') {
+    currentID++;
     getPokemon();
     rightAnswer = false;
     event.target.value = null;
     $answerBox.className = 'answer-input';
   }
+}
+
+
+
+function shuffle(min, max) {
+  var array = [];
+  for (min; min <= max; min++) {
+    array.push(min);
+  }
+  var i = array.length;
+  var randomVal = 0;
+  var hold;
+
+  while (i--) {
+    randomVal = Math.floor(Math.random() * (i + 1));
+
+    hold = array[i];
+    array[i] = array[randomVal];
+    array[randomVal] = hold;
+  }
+  return array;
 }
